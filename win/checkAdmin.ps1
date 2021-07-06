d3768f86b1a525eaf51b1683b05ed079d9d77073
carlstewart222 <48135585+carlstewart222@users.noreply.github.com>
2021-07-06 12:08:16 -0400

    Add files via upload

---
+++ win/checkAdmin.ps1
@@ -0,0 +1,7 @@
+﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+ {    
+  Echo "This script needs to be run As Admin"
+  Break
+ }
+
+ echo "Do Something"
